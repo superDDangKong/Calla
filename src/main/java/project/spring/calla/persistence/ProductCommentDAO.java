2643d14823c7b94e375affb57f001eb605df778d
superDDangKong <140034486+superDDangKong@users.noreply.github.com>
2023-10-26 11:45:58 +0900

    3차 병합

--- src/main/java/project/spring/calla/persistence/ProductCommentDAO.java
+++ src/main/java/project/spring/calla/persistence/ProductCommentDAO.java
@@ -6,11 +6,11 @@ import project.spring.calla.domain.ProductCommentVO;
 import project.spring.calla.pageutil.PageCriteria;
 
 public interface ProductCommentDAO {
-	int insert(ProductCommentVO vo); // ��� ���
-	List<ProductCommentVO> select(int productId); // ��ǰ�� ��� �˻�
-	int update(int productCommentId, String productCommentContent); // ��� ����
-	int delete(int productCommentId); // ��� ����
-	List<ProductCommentVO> select(PageCriteria criteria);
-	int getTotalCount();
+	int insert(ProductCommentVO vo); // 占쏙옙占� 占쏙옙占�
+	List<ProductCommentVO> select(int productId); // 占쏙옙품占쏙옙 占쏙옙占� 占싯삼옙
+	int update(int productCommentId, String productCommentContent); // 占쏙옙占� 占쏙옙占쏙옙
+	int delete(int productCommentId); // 占쏙옙占� 占쏙옙占쏙옙
+	List<ProductCommentVO> select(PageCriteria criteria, int productId);
+	int getTotalCount(int productId);
 	List<ProductCommentVO> select(String memberNickname);
 }
